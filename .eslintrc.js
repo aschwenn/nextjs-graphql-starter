93fe200ef32998382e7a166ff56ff3caa4885d26
Andrew Schwenn <aschwenn@verizon.net>
2021-05-26 20:18:03 -0500

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true
+  },
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ]
+}
